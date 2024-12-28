789d08032ed72dadab477982cc96eb1901260225
Jhojan Mendoza <Jhojanbautista11@gmail.com>
2024-12-27 19:56:44 -0500

    Primer despliegue

--- registro_horas/settings.py
+++ registro_horas/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = 'django-insecure-jm3w=q5d=j^pivc&*%xccj0lt-v234ac9#2u8pj(ux@^8edp=7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'horariotimer-production.up.railway.app']
 
 # Application definition
 
